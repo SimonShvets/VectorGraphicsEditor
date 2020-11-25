0964fdc58ae96093c01d5bbb9f31c57a2cf98677
SimonShvets <goodweather.spb@gmail.com>
2020-11-25 21:11:56 +0300

    Create Structures

--- EditorForm.cs
+++ EditorForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VectorGraphicsEditor.Figures;
 
 namespace VectorGraphicsEditor
 {
@@ -15,14 +17,15 @@ namespace VectorGraphicsEditor
         Bitmap mainBitmap;
         Graphics graphics;
         Pen pen;
-        Image image;
+        List<Point> listOfPoints = new List<Point>();
+        Bitmap tmpBitmap;
         Point point;
         bool mouseDown;
-        bool needClear;
         int chooseButton;
         int x;
         int y;
-
+        IFigures figure;
+        Point[] g = new Point[2];
 
         public EditorForm()
         {
@@ -33,63 +36,65 @@ namespace VectorGraphicsEditor
         {
             mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             graphics = Graphics.FromImage(mainBitmap);
-            pen = new Pen(Color.Black, 10);
+            pen = new Pen(Color.Black, 2);
             pictureBox1.Image = mainBitmap;
-            point = new Point(0, 0);
+            figure = new CurveFigure();
             mouseDown = false;
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            x = e.X;
-            y = e.Y;
             point = e.Location;
+            listOfPoints.Add(point);
+            Point[] g = new Point[listOfPoints.Count];
+            int k = 0;
+            foreach (Point i in listOfPoints)
+            {
+                g[k] = i;
+                k++;
+            }
+            if (chooseButton == 2)
+            {
+                pictureBox1.Image = figure.DrawFigure(tmpBitmap, g);
+            }
             mouseDown = true;
+
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (mouseDown)
-            {
 
-                switch (chooseButton)
-                {
-                    case (0):
-                        break;
-                    case (1):
-                        graphics.DrawLine(pen, point, e.Location);
-                        pictureBox1.Image = mainBitmap;
-                        point = e.Location;
-                        break;
-                    case (2):
-                        graphics.Clear(Color.White);
-                        graphics.DrawLine(pen, point, e.Location);
-                        pictureBox1.Image = mainBitmap;
-                        needClear = true;
-                        break;
-                    case (3):
-                        graphics.Clear(Color.White);
-                        PointF[] points = new PointF[4] { point, new Point(point.X, e.Y), e.Location, new Point(e.X, point.Y) };
-                        graphics.DrawPolygon(pen, points);
-                        pictureBox1.Image = mainBitmap;
-                        needClear = true;
-                        break;
-                    case (5):
-                        graphics.Clear(Color.White);
-                        Rectangle rect = new Rectangle(x, y, e.X-x, e.Y-y);
-                        graphics.DrawEllipse(pen, rect);
-                        pictureBox1.Image = mainBitmap;
-                        needClear = true;
-                        break;
-                }
-
-            }
-
+                //switch (chooseButton)
+                //{
+                //    case (0):
+                //        break;
+                //    case (1):
+                //        graphics.DrawLine(pen, point, e.Location);
+                //        pictureBox1.Image = mainBitmap;
+                //        point = e.Location;
+                //        break;
+                //    case (2):
+                //        break;
+                //    case (3):
+                //        graphics.Clear(Color.White);
+                //        PointF[] points = new PointF[4] { point, new Point(point.X, e.Y), e.Location, new Point(e.X, point.Y) };
+                //        graphics.DrawPolygon(pen, points);
+                //        pictureBox1.Image = mainBitmap;
+                //        break;
+                //    case (5):
+                //        graphics.Clear(Color.White);
+                //        Rectangle rect = new Rectangle(x, y, e.X-x, e.Y-y);
+                //        graphics.DrawEllipse(pen, rect);
+                ////        pictureBox1.Image = mainBitmap;
+                ////        break;
+                //}
+                tmpBitmap = new Bitmap(mainBitmap);
+                graphics = Graphics.FromImage(tmpBitmap);
+                pictureBox1.Image = figure.DrawFigure(tmpBitmap, g);
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            needClear = false;
             mouseDown = false;
         }
 
@@ -104,8 +109,8 @@ namespace VectorGraphicsEditor
         private void createLine_Click(object sender, EventArgs e)
         {
             pictureBox1.Cursor = Cursors.Cross;
+            figure = new CurveFigure();
             chooseButton = 2;
-
         }
 
         private void rectangle_Click(object sender, EventArgs e)
