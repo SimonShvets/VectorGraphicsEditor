5c270b09e5ce70a1fd23686b4e7f38953d777f8b
Даниил <evkakiy.olen@gmail.com>
2020-11-30 10:25:48 +0300

    squareAndTriangles

--- Figures/IsoscelesTriangleFigure.cs
+++ Figures/IsoscelesTriangleFigure.cs
@@ -11,7 +11,22 @@ namespace VectorGraphicsEditor.Figures
     {
         public void DrawFigure(Pen pen, Graphics graphics, PointList pointList)
         {
+            PointF[] points = new PointF[3];
+           
+                points[0] = new PointF(pointList[0].X, pointList[1].Y);
+                int a = 360 / 3;
+                float t;
+                for (int i = 1; i < 3; i++)
+                {
+                    t = a * i;
+                    float rad = (float)(Math.PI / 180.0 * t);
+                    float x1 = (float)((pointList[0].X - (points[0].Y - pointList[0].Y) * Math.Sin(rad)));
+                    float y1 = (float)((pointList[0].Y + (points[0].Y - pointList[0].Y) * Math.Cos(rad)));
+                    points[i] = new PointF(x1, y1);
+                }
             
+            graphics.DrawPolygon(pen, points);            
+
         }
     }
 }
---
+++ Figures/SquareFigure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace VectorGraphicsEditor.Figures
+{
+    public class SquareFigure : IFigure
+    {
+        public void DrawFigure(Pen pen, Graphics graphics, PointList pointList)
+        {
+            PointF[] points = new PointF[4];
+            int x = (int)pointList[0].X;
+            int y = (int)pointList[0].Y;
+            int x2 = (int)pointList[1].X;
+           // int y2 = (int)pointList[1].Y;
+            int a = Math.Abs(x2 - x);
+           // int b = Math.Abs(y2 - y);
+            //int c = a + b;
+            points[0] = pointList[0];
+            //points[1] = new PointF(pointList[0].X, pointList[1].Y + a);
+            //points[2] = pointList[1];
+            //points[3] = new PointF(pointList[1].X + a, pointList[0].Y);
+            points[1] = new PointF(x, y + a);
+            points[2] = new PointF(x + a, y + a);
+            points[3] = new PointF(x + a, y);
+            graphics.DrawPolygon(pen, points);
+            
+        }
+    }
+}
